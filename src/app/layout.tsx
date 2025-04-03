2b2c00087434ee5aa123f09ca8d7b4361fab6e25
jyeongpark <jiyeongstar@gmail.com>
2025-04-03 11:59:00 +0900

    레이아웃 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <div>Global Layout</div>
         {children}
       </body>
     </html>
