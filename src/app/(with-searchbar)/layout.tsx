f4c2a0b0db804fc3b0d36feaf7e81403019f5ea0
jyeongpark <jiyeongstar@gmail.com>
2025-04-09 11:12:05 +0900

    폴 라우트

--- src/app/(with-searchbar)/layout.tsx
+++ src/app/(with-searchbar)/layout.tsx
@@ -1,10 +1,13 @@
 import Searchbar from "@/components/searchbar";
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
-      <Searchbar />
+      <Suspense fallback={<div>Loading...</div>}>
+        {/* Suspense는 fallback을 사용하여 로딩 중인 상태를 보여줄 수 있다. */}
+        <Searchbar />
+      </Suspense>
       {children}
     </div>
   );
--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,5 +1,26 @@
 import { MovieData } from "@/types";
 import style from "./page.module.css";
+import { notFound } from "next/navigation";
+
+export const dynamicParams = false; // generateStaticParams에 명시 안한 값으로 접속 요청하면 404
+
+export async function generateStaticParams() {
+  // 정적인 parameter를 생성해주는 함수
+  // getStaticPaths와 유사한 역할을 함
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`,
+    { cache: "force-cache" }
+  );
+  // force-cache 저장된 데이터 캐싱.
+  // 데이터 베이스가 바뀌지 않는 한 같은 결과를 보여주기 때문
+  if (!response.ok) {
+    return [];
+  }
+
+  const movies: MovieData[] = await response.json();
+  return movies.map((movie) => ({ id: String(movie.id) })); // params로 보낼 id들을 string으로 변환
+}
 
 export default async function Page({
   params,
@@ -17,7 +38,7 @@ export default async function Page({
   // 데이터 베이스가 바뀌지 않는 한 같은 결과를 보여주기 때문
 
   if (!response.ok) {
-    return <div>오류가 발생했습니다.</div>;
+    return notFound();
   }
 
   const movie: MovieData = await response.json();
